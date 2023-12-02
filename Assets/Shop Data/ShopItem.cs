2daeb7ff11c207a47a0549f150f3476b92b8913c
Cláuvin <Clauvin@users.noreply.github.com>
2023-12-01 23:05:34 -0300

    [buy-sell-store] More repositioning

---
+++ Assets/Shop Data/ShopItem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Shop/Item", fileName = "ShopItem_")]
+public class ShopItem : ScriptableObject
+{
+    public ShopItemCategory category;
+    public string name;
+    [TextArea(3, 5)] public string description;
+
+    public int cost;
+}
