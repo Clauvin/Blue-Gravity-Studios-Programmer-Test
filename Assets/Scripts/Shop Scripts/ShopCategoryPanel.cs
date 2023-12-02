a7b86dec8490d15a6636abd7eb55d75289aa48ea
Cláuvin <Clauvin@users.noreply.github.com>
2023-12-01 23:22:06 -0300

    [buy-sell-store] updated ShopCategoryPanel.cs

--- Assets/Scripts/Shop Scripts/ShopCategoryPanel.cs
+++ Assets/Scripts/Shop Scripts/ShopCategoryPanel.cs
@@ -2,15 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class ShopCategoryPanel : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI categoryName;
+    [SerializeField] Image backgroundPanel;
+    [SerializeField] UnityEngine.Color defaultColor;
+    [SerializeField] UnityEngine.Color selectedColor;
     ShopItemCategory panelCategory;
+    UnityAction<ShopItemCategory> onSelectFunction;
 
-    public void Bind(ShopItemCategory category)
+    public void Bind(ShopItemCategory category, UnityAction<ShopItemCategory> onSelectedFunctionDoThis)
     {
         panelCategory = category;
         categoryName.text = category.Name;
+        onSelectFunction = onSelectedFunctionDoThis;
+
+        SetIsSelected(false);
+    }
+
+    public void SetIsSelected(bool selected)
+    {
+        backgroundPanel.color = selected ? selectedColor : defaultColor;
+    }
+
+    public void OnClicked()
+    {
+
     }
 }
