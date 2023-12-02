0b48f81b2d655c5dd3435addecb3de86f59d20ad
Cláuvin <Clauvin@users.noreply.github.com>
2023-12-01 23:05:10 -0300

    [buy-sell-store] A lot of name changing and script repositioning

---
+++ Assets/Scripts/Shop Scripts/ShopUI.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShopUI : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI availableFunds;
+    [SerializeField] Transform categoryUIRoot;
+    [SerializeField] Transform itemUIRoot;
+
+    [SerializeField] GameObject categoryUIPrefab;
+    [SerializeField] GameObject itemUIPrefab;
+
+    [SerializeField] List<ShopItem> availableItems;
+
+    List<ShopItemCategory> shopCategories;
+    Dictionary<ShopItemCategory, ShopCategoryPanel> shopCategoryToUIMap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RefreshShopUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void RefreshShopUI()
+    {
+        shopCategories = new List<ShopItemCategory>();
+        shopCategoryToUIMap = new Dictionary<ShopItemCategory, ShopCategoryPanel>();
+        foreach(var item in availableItems)
+        {
+            if (!shopCategoryToUIMap.ContainsKey(item.category))
+            {
+                shopCategories.Add(item.category);
+            }
+        }
+
+        shopCategories.Sort((left, right) => left.Name.CompareTo(right.Name));
+
+        foreach(var category in shopCategories)
+        {
+            GameObject categoryGo = Instantiate(categoryUIPrefab, categoryUIRoot);
+            ShopCategoryPanel categoryUI = categoryGo.GetComponent<ShopCategoryPanel>();
+
+            categoryUI.Bind(category);
+            shopCategoryToUIMap[category] = categoryUI; 
+        }
+    }
+
+    public void OnClickedPurchase()
+    {
+
+    }
+}
