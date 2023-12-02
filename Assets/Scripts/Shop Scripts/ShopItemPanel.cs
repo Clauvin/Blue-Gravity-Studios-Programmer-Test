280e97f92c7d4dd4b7b6cebead348600d762ed22
Cláuvin <Clauvin@users.noreply.github.com>
2023-12-02 01:42:03 -0300

    [buy-sell-store] ShopItemPanel.cs now has code similar to ShopCategoryPanel.cs

--- Assets/Scripts/Shop Scripts/ShopItemPanel.cs
+++ Assets/Scripts/Shop Scripts/ShopItemPanel.cs
@@ -2,23 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class ShopItemPanel : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI itemName;
     [SerializeField] TextMeshProUGUI description;
     [SerializeField] TextMeshProUGUI price;
+    [SerializeField] Image backgroundPanel;
+    [SerializeField] Color defaultColor;
+    [SerializeField] Color selectedColor;
+    UnityAction<ShopItem> onSelectedFunction;
 
     ShopItem panelItem;
 
-    public void Bind(ShopItem item)
+    public void Bind(ShopItem item, UnityAction<ShopItem> onSelectedFunctionDoThis)
     {
         panelItem = item;
 
         itemName.text = item.name;
         description.text = item.description;
         price.text = item.cost.ToString();
+
+        SetIsSelected(false);
     }
 
+    public void SetIsSelected(bool selected)
+    {
+        backgroundPanel.color = selected ? selectedColor : defaultColor;
+    }
+
+    public void OnClicked()
+    {
+        onSelectedFunction.Invoke(panelItem);
+    }
 
 }
