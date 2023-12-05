1b385269d0dd2ebe30232d978ceea5b149b42613
Cláuvin <Clauvin@users.noreply.github.com>
2023-12-04 21:21:25 -0300

    [buy-sell-store] Added resaleVariableCost to ShopItem

--- Assets/Shop Data/ShopItem.cs
+++ Assets/Shop Data/ShopItem.cs
@@ -10,4 +10,17 @@ public class ShopItem : ScriptableObject
     [TextArea(3, 5)] public string description;
 
     public int cost;
+    //debt: works, but honestly we don't need this variable to have a resale cost, the SellingShopUI can take care of it as long as it does not
+    //  changes the cost variable value
+    private int resaleVariableCost;
+
+    public int GetResaleVariableCost()
+    {
+        return resaleVariableCost;
+    }
+
+    public void SetResaleVariableCost(int newResaleVariableCost)
+    {
+        resaleVariableCost = newResaleVariableCost;
+    }
 }
